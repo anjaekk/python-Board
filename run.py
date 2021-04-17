20966eefa90b5ef3a686c0bf0d6dda21d12adef2
Jaegyeong-An <anjaekk@gmail.com>
2021-04-17 14:57:17 +0900

    Create search function

--- run.py
+++ run.py
@@ -33,11 +33,37 @@ def lists():
     # 한 페이지당 몇 개의 게시물을 출력할지
     limit = request.args.get("limit", 7, type=int)
 
+    search = request.args.get("search", -1, type=int)
+    keyword = request.args.get("keyword", type=str)
+
+    # 최종적으로 완성된 쿼리를 만들 변수
+    query = {}
+    # 검색어 상태를 추가할 리스트 변수
+    search_list = []
+
+    if search == 0:
+        search_list.append({"title": {"$regex" : keyword}})
+    elif search == 1:
+        search_list.append({"contents": {"$regex" : keyword}})
+    elif search == 2:
+        search_list.append({"title": {"$regex" : keyword}})       
+        search_list.append({"contents": {"$regex" : keyword}})
+    elif search == 2:
+        search_list.append({"name": {"$regex" : keyword}})       
+    
+    # 검색 대상이 한개라도 존재할 경우 query 변수에 $or 리스트를 쿼리 함
+    if len(search_list) > 0:
+        query = {"$or": search_list}
+    
+
+    print(query)
+
+
     board = mongo.db.board
-    datas = board.find({}).skip((page - 1) * limit).limit(limit)
+    datas = board.find(query).skip((page - 1) * limit).limit(limit)
 
     # 게시물의 총 갯수
-    tot_count = board.find({}).count()
+    tot_count = board.find(query).count()
     # 마지막 페이지의 수를 구한다.
     last_page_num = math.ceil(tot_count / limit)
 
@@ -57,13 +83,19 @@ def lists():
         page=page,
         block_start=block_start,
         block_last=block_last,
-        last_page_num=last_page_num)
+        last_page_num=last_page_num,
+        search=search,
+        keyword=keyword) 
 
 
 @app.route("/view/<idx>")
 def board_view(idx):
     # idx = request.args.get("idx")
     if idx is not None:
+        page = request.args.get("page")
+        search = request.args.get("search")
+        keyword = request.args.get("keyword")
+
         board = mongo.db.board
         data = board.find_one({"_id": ObjectId(idx)})
 
@@ -77,7 +109,7 @@ def board_view(idx):
                 "view": data.get("view"),
             }
 
-            return render_template("view.html", result=result)
+            return render_template("view.html", result=result, page=page, search=search, keyword=keyword)
     return abort(404)
         
 
