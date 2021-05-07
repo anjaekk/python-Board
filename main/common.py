79d803a2bc834e526a8d2e4677f557019231e579
anjaekk <anjaekk@gmail.com>
2021-05-07 13:50:38 +0900

    Modularization

---
+++ main/common.py
@@ -0,0 +1,12 @@
+from functools import wraps
+from main import session, redirect, request, url_for
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("id") is None or session.get("id") == "":
+            # next_url에 현재 주소값인 request.url넘겨줌
+            return redirect(url_for("member_login", next_url=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
---
+++ main/member.py
@@ -0,0 +1,77 @@
+from main import *
+
+@app.route("/join", methods=["GET", "POST"])
+def member_join():
+    if request.method == "POST":
+        name = request.form.get("name", type=str)
+        email = request.form.get("email", type=str)
+        pass1 = request.form.get("pass", type=str)
+        pass2 = request.form.get("pass2", type=str)
+
+        if name == "" or email == "" or pass1 == "" or pass2 == "":
+            flash("입력되지 않은 값이 있습니다.")
+            return render_template("join.html") # join으로 다시 복귀
+  
+        if pass1 != pass2:
+            flash("비밀번호가 일치하지 않습니다.")
+            return render_template("join.html") # join으로 다시 복귀
+
+        members = mongo.db.members
+        cnt = members.find({"email": email}).count()
+        if cnt > 0:
+            flash("중복된 이메일 주소입니다.")
+            return render_template("join.html") # join으로 다시 복귀
+        
+
+        current_utc_time = round(datetime.utcnow().timestamp() * 1000)
+        post = {
+            "name": name,
+            "email": email,
+            "pass": pass1,
+            "joindate": current_utc_time,
+            "logintime": "",
+            "logincount": 0
+        }
+
+        members.insert_one(post)
+
+        return ""
+    else:
+        return render_template("join.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def member_login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("pass")
+        next_url = request.form.get("next_url")
+
+        members = mongo.db.members
+        data = members.find_one({"email": email})
+
+        if data is None:
+            flash("회원 정보가 없습니다.")
+            # GET메소드로 login.html 재 실행
+            return redirect(url_for("member_login"))
+        else:
+            if data.get("pass") == password:
+                session["email"] = email
+                session["name"] = data.get("name")
+                session["id"] = str(data.get("_id"))
+                # 세션 유지시간 설정함
+                session.permanent = True
+                if next_url is not None:
+                    return redirect(next_url)
+                else:
+                    return redirect(url_for("lists"))
+            else:
+                flash("비밀번호가 일치하지 않습니다.")
+                return redirect(url_for("member_login"))
+        return ""
+    else:
+        next_url = request.args.get("next_url", type=str)
+        if next_url is not None:
+            return render_template("login.html", next_url=next_url)
+        else:
+            return render_template("login.html")
